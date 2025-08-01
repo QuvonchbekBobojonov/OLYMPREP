9f25eefe842374a20d273ea2dc5167dfc366e353
Quvonchbek Bobojonov <hi@moorfo.uz>
2025-08-01 10:30:41 +0500

    feat: create models

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Olympics, OlympicsCategory, Mentor
+
+@admin.register(OlympicsCategory)
+class OlympicsCategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Mentor)
+class MentorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Olympics)
+class OlympicsAdmin(admin.ModelAdmin):
+    pass
--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class OlympicsCategory(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=100)
+    experience = models.IntegerField()
+    achieved = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Olympics(models.Model):
+    category = models.ForeignKey(OlympicsCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    info = models.TextField()
+    mentors = models.ManyToManyField(Mentor)
+    resources = models.TextField()
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['category']
+        verbose_name_plural = 'Olympics'
+        verbose_name = 'Olympics'
+
